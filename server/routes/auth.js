c3a71cbe2efa1dd9c25692c5d03fe053efc2de80
Naveen Kongara <kongara.naveen97@gmail.com>
2025-08-03 15:42:19 +0530

    Updated auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const Database = require('../database/init');
+//const Database = require('../database/init');
 const { generateTokens, JWT_REFRESH_SECRET } = require('../middleware/auth');
 
 const router = express.Router();
