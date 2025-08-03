9b1ce8f52e2b9e9149464bb7db27ba9e89d04062
Naveen Kongara <kongara.naveen97@gmail.com>
2025-08-03 15:42:25 +0530

    Updated admin.js

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const axios = require('axios');
-const Database = require('../database/init');
+//const Database = require('../database/init');
 const { authenticateAdmin } = require('../middleware/auth');
 
 const router = express.Router();
